bd76c08cfb2e381d0e40bb2dd117868b46ea3c17
alaaaldeen <33576652+alaaaldeen@users.noreply.github.com>
2018-02-01 10:29:39 +0400

    Create arithmetic.py

---
+++ Socratica/arithmetic.py
@@ -0,0 +1,17 @@
+a = 2 # int
+b = 3L # long
+c = 6.0 # float
+d = 12 + 0j # complex number
+# Rule: Widen numbers so they're the same type
+
+# Addition 
+a + b   # int + long
+
+# Subtraction 
+c - b   # float - long
+
+# Multiplication
+a * c    # int * float
+
+# Division
+d / c   # complex / float
