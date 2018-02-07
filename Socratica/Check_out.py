298311bec768fb5a64132f253b7fdb971532f95b
Alaa Alskhney <o-aahasan@adhsc.seha.ae>
2018-02-07 11:54:59 +0400

    changed module datetime to date

--- Socratica/Check_out.py
+++ Socratica/Check_out.py
@@ -1,7 +1,7 @@
 # excersizing datetime timedelta object
 
 import datetime
-checkin = datetime.datetime(2017, 7, 2)
+checkin = datetime.date(2017, 7, 2)
 stay = datetime.timedelta(90)
 checkout = checkin + stay
 print checkout
