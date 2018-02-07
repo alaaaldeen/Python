c96f4460da49a19e75cd4247878c45ce5c8d4b66
Alaa Alskhney <o-aahasan@adhsc.seha.ae>
2018-02-07 12:59:09 +0400

    formating text to datetime object

--- Socratica/Check_out.py
+++ Socratica/Check_out.py
@@ -5,3 +5,9 @@ checkin = datetime.date(2017, 7, 2)
 stay = datetime.timedelta(90)
 checkout = checkin + stay
 print checkout
+
+# Converting string into datetime object
+moon_landing = "7/20/1969"
+moon_landing_datetime = datetime.datetime.strptime(moon_landing, "%m/%d/%Y")
+print(moon_landing_datetime)
+print(moon_landing_datetime.strftime("%A, %B %d, %Y"))
