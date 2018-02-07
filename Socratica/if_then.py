e1379046777beefbd4a92afe9388c4487eca0931
Alaa Alskhney <o-aahasan@adhsc.seha.ae>
2018-02-07 13:40:41 +0400

    Triangle Type

---
+++ Socratica/if_then.py
@@ -0,0 +1,14 @@
+# Scalene triangle: All sides have different lengths.
+# Isosceles triangle: Two sides have the same lengths.
+# Equilateral triangle: All sides are equal.
+
+a = int(raw_input("The length of side a = "))
+b = int(raw_input("The length of side b = "))
+c = int(raw_input("The length of side c = "))
+
+if a != b and b != c and a != c:
+    print("This is a Scalene triangle.")
+elif a == b and b == c:
+    print("This is an Equilateral triangle.")
+else:
+    print("This is an Isosceles triangle.")
