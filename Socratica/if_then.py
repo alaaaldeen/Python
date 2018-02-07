db337681ced57acb4d23ab625a803cc6da470da7
Alaa Alskhney <o-aahasan@adhsc.seha.ae>
2018-02-07 15:03:47 +0400

    triangle formula

--- Socratica/if_then.py
+++ Socratica/if_then.py
@@ -6,9 +6,12 @@ a = int(raw_input("The length of side a = "))
 b = int(raw_input("The length of side b = "))
 c = int(raw_input("The length of side c = "))
 
-if a != b and b != c and a != c:
-    print("This is a Scalene triangle.")
-elif a == b and b == c:
-    print("This is an Equilateral triangle.")
+if a + b > c and a + c > b and b + c > a:
+    if a != b and b != c and a != c:
+        print("This is a Scalene triangle.")
+    elif a == b and b == c:
+        print("This is an Equilateral triangle.")
+    else:
+        print("This is an Isosceles triangle.")
 else:
-    print("This is an Isosceles triangle.")
+    print("The provided numbers doent form a triangle")
