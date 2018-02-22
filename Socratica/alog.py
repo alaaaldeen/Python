a9bc5a84724186cfef7d311d797bdf66d06659bd
Alaa Alskhney <o-aahasan@adhsc.seha.ae>
2018-02-22 14:35:32 +0400

    comments explain creating logger process

--- Socratica/alog.py
+++ Socratica/alog.py
@@ -1,13 +1,15 @@
 import logging
 
-# Create and configure logger
+# Configure the logging system
+# First configure the sataus messages or trail stream
 LOG_FORMAT = "%(levelname)s %(asctime)s - %(message)s"
-
+# Configure logging settings
 logging.basicConfig(filename="D:\\Script\\lumberjack.log",
                     # Debug level to display all
                     level=logging.DEBUG,
                     format=LOG_FORMAT,
                     filemode='w')
+# Creating logger object by calling getLogger method
 logger = logging.getLogger()
 
 # Test messages
